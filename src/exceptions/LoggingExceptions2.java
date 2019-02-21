f42bc2b7de873bac7a1b116e5ad52d7ad98bcea6
1119674493@qq.com <1119674493@qq.com>
2019-02-21 22:32:16 +0800

    异常处理

---
+++ src/exceptions/LoggingExceptions2.java
@@ -0,0 +1,21 @@
+package exceptions;
+//捕获和记录其他人编写的异常
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+public class LoggingExceptions2 {
+	private static Logger logger = Logger.getLogger("LoggingExceptions2");
+	static void logException(Exception e) {
+		StringWriter trace = new StringWriter();
+		e.printStackTrace(new PrintWriter(trace));
+		logger.severe(trace.toString());
+	}
+	public static void main(String[] args) {
+		try {
+			throw new NullPointerException();
+		} catch (NullPointerException e) {
+			logException(e);
+		}
+	}
+}
