ca7f2fc2c4d03cf007f94ad5de73337c21c0b7bf
1119674493@qq.com <1119674493@qq.com>
2019-03-05 21:11:52 +0800

    线程

---
+++ src/concurrency/LiftOff.java
@@ -0,0 +1,25 @@
+package concurrency;
+
+public class LiftOff implements Runnable {
+
+	protected int countDown = 10;
+	private static int taskCount = 0;
+	private final int id = taskCount++;
+	public LiftOff() {}
+	public LiftOff(int countDown) {
+		this.countDown = countDown;
+	}
+	public String status() {
+		return "#" + id + "(" + 
+				(countDown > 0 ? countDown : "Liftoff!") + "), ";
+	}
+	
+	@Override
+	public void run() {
+		// TODO 自动生成的方法存根
+		while(countDown-- > 0) {
+			System.out.print(status());
+			Thread.yield();
+		}
+	}
+}
